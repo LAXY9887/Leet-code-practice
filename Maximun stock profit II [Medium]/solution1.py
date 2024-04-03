f45b28842b32e7111839c95b3f676f7d4747570c
LAXY9887 <lxya98874322688423@protonmail.com>
2024-04-03 09:45:14 +0800

    Update practice 2024-04-02

--- Maximun stock profit II [Medium]/solution1.py
+++ Maximun stock profit II [Medium]/solution1.py
@@ -30,37 +30,55 @@ Output: 0
 Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
 
 """
+"""
 
-price = [1,2,4,2,5,7,2,4,9,0,9]
+solution (Greedy algorism)
 
+    1. This method optimize the local best solution, and hope for achieve the global optimiztion
+
+    2. Although it often failed to be the best global solution, the outcome is somewhat useful.
+
+    3. The method is simply sell the stock while it can make profit.
+
+    4. Initialize time pointer p1 at 0 and p2 at 1, current profit = 0 and max profit (profit) = 0
+
+    5. Loop through the price array and check the price of p2 is grater than p1, if so, buy and sell it,
+       calculating current profit and add max profit by it.
+
+    6. No matter the deal is made, update the time point p1 to p2. (Because we dont care about the global max profit)
+
+    7. Update the p2 time point by 1.
+
+    8. Max profit will be accumulated by local best deals.
+
+"""
+
+
+# Input
+prices = [1,2,4,2,5,7,2,4,9,0,9]
+
+# Time pointer
 p1 = 0
 p2 = 1
 
+# Record profits
 current_profit = 0
-next_profit = 0
 profit = 0
-hold = False
 
-for i in range(len(price)-1):
+for i in range(len(prices)-1):
 
-    if price[p2] > price[p1]:
-        current_profit = price[p2] - price[p1]
-        hold = True
-        
-        if p2+1 <= len(price)-1:
-            next_profit = price[p2+1] - price[p1]
-        else:
-            if hold:
-                profit += current_profit
-            
-        if next_profit < current_profit and hold:
-            profit += current_profit
-            hold = False
-            p1 = p2
+    # if prices at p2 > p1, buy and hold it, calculate current profit
+    if prices[p2] > prices[p1]:
+        current_profit = prices[p2] - prices[p1]
 
-    else:
-        p1 = p2
+        # Add profit by current_profit
+        # Update pointer p1 to p2
+        profit += current_profit
 
+    # Update p1 to p2 time point
+    p1 = p2
+
+    # Update p2 to next time point
     p2 += 1
 
 print(profit)
